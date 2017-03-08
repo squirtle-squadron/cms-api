8eaf614481335ec544cfbd3d558201be032d2d02
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-08 13:04:16 -0500

    Create show action
    Create curl request, routes and controller for show actions that works
    However we are not definately sure because currently we don't have
    anything in database.

--- config/routes.js
+++ config/routes.js
@@ -16,6 +16,6 @@ module.exports = require('lib/wiring/routes')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
-
+.resources('blogs', {only:['show']})
 // all routes created
 ;
