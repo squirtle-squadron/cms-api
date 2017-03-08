cc55ee518d875f698c72b4cec4e71be0a6d4eff9
ashlenprice <ashlen.price@gmail.com>
2017-03-08 15:38:22 -0500

    attempt to fix rebase conflicts

--- config/routes.js
+++ config/routes.js
@@ -16,7 +16,6 @@ module.exports = require('lib/wiring/routes')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
-.resources('blogs', { only: ['index', 'show', 'create', 'update', 'destroy'] })
 
 // all routes created
 ;
